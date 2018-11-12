2054d0bb6847320a63d7fb2594196e2880e746ae
LILIT <lilit555ai@gmail.com>
2018-11-11 23:08:38 -0700

    Add writing to cloud storage

--- data_to_csv.py
+++ data_to_csv.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import tensorflow as tf
 import os
+import datalab.storage as gcs
 
 labels = ['food','not food']
-datset = ['TRAIN', 'Validation', 'TEST']
+dataset = ['VALIDATION', 'TEST', 'TRAIN']
 data = []
 for s in dataset:
-    path = os.path.join('gs://food-resnet/dataset/',s)
+    path = os.path.join('gs://food_resnet/dataset/',s)
     for l in labels:
-        image_path = tf.gfile.Glob(os.path.join(path, l)):
-            for file_path in tf.gfile.Glob(os.path.join(image_path,'/*')):
-                data.append(s, file_path, l)
+        for file_path in tf.gfile.Glob(os.path.join(path, l+'/*')):
+            data.append((s, file_path, l))
 
 pd_data = pd.DataFrame(data)
-pd_data.to_csv('data.csv', index=False, header=False, )
+gcs.Bucket('food_resnet').item('dataset/data.csv').write_to(pd_data.to_csv(index=False, header=False),'text/csv')
